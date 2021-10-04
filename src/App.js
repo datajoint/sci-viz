aceef155e3642b7788779a1bc11ed89464611485
jverswijver <jeroen.verswijver@gmail.com>
2021-10-04 15:26:00 -0500

    Add basic table from a tutorial.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
+import Table from './Components/Table';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1><Table/></h1>
       </header>
     </div>
   );
