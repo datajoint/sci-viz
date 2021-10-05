653bd9d4eb211fb42912fbdd13125c5cf787c347
jverswijver <jeroen.verswijver@gmail.com>
2021-10-05 11:25:31 -0500

    Basic Login framework and typescript support.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
 import Table from './Components/Table';
+import Login from './Components/Login/Login';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1><Table/></h1>
+        <h2><Login/></h2>
       </header>
     </div>
   );
