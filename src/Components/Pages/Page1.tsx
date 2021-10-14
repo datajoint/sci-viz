4071c4f76a9e5fe560e3fc57d328e23c2d947524
jverswijver <jeroen.verswijver@gmail.com>
2021-10-14 16:28:54 -0500

    Add python script to generate pages.

--- src/Components/Pages/Page1.tsx
+++ src/Components/Pages/Page1.tsx
@@ -2,21 +2,13 @@ import React from 'react';
 import TableView from '../Table/TableView';
 import GridLayout from 'react-grid-layout';
 import SideBar from '../SideBar/SideBar';
-// Component imports
-import './Page1.css'
+import './Page.css'
 
 interface Page1Props {
   jwtToken: string;
 }
 
-const layout = [
-    {i: 'table1', x: 0, y: 0, w: 1, h: 1, static: true},
-    {i: 'table2', x: 1, y: 0, w: 1, h: 1, static: true},
-    {i: 'table3', x: 2, y: 0, w: 1, h: 1, static: true},
-  ];
-  
-  
-  
+   
   /**
    * Main view for DJGUI App
    */
@@ -26,18 +18,36 @@ const layout = [
         <div>
           <div className='grid-container'>
             <SideBar />
-            <GridLayout className="mygrid" layout={layout} cols={3} rowHeight={400} width={1600}>
-              <div key='table1'>
-              <TableView token={this.props.jwtToken} route='/query1' tableName='test1'/>
-              </div>
-              <div key='table2'>
-              <TableView token={this.props.jwtToken} route='/query1' tableName='test2'/>
-              </div>
-              <div key='table3'>
-              <TableView token={this.props.jwtToken} route='/query5' tableName='test3'/>
-              </div>
-            </GridLayout>
+            <ul>
+              <li style={{display: 'block'}}>
+                <GridLayout className="mygrid" cols={ 3 }  rowHeight={400} width={1600}>
+                  <div key='table1' data-grid={{x: 0, y: 0, w: 1, h: 1, static: true}}>
+                  <TableView token={this.props.jwtToken} route='/query1' tableName='test1'/>
+                  </div>
+                  <div key='table2' data-grid={{x: 1, y: 0, w: 1, h: 1, static: true}}>
+                  <TableView token={this.props.jwtToken} route='/query1' tableName='test2'/>
+                  </div>
+                  <div key='table3' data-grid={{x: 2, y: 0, w: 1, h: 1, static: true}}>
+                  <TableView token={this.props.jwtToken} route='/query5' tableName='test3'/>
+                  </div>
+                </GridLayout>
+              </li>
+              <li style={{display: 'block'}}>
+                <GridLayout className="mygrid" cols={3} rowHeight={400} width={1600}>
+                  <div key='table1' data-grid={{x: 0, y: 0, w: 1, h: 1, static: true}}>
+                  <TableView token={this.props.jwtToken} route='/query1' tableName='test1'/>
+                  </div>
+                  <div key='table2' data-grid={{x: 1, y: 0, w: 1, h: 1, static: true}}>
+                  <TableView token={this.props.jwtToken} route='/query1' tableName='test2'/>
+                  </div>
+                  <div key='table3' data-grid={{x: 2, y: 0, w: 1, h: 1, static: true}}>
+                  <TableView token={this.props.jwtToken} route='/query5' tableName='test3'/>
+                  </div>
+                </GridLayout>
+              </li>
+            </ul>
           </div>
+          
         </div>
       )
     }
