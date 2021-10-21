3c6d9f09ac18cdc9111d11cf06b9d22997eb88a9
jverswijver <jeroen.verswijver@gmail.com>
2021-10-21 15:38:50 -0500

    Add Sidebar collapse feature.

--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -2,11 +2,13 @@ import './SideBar.css'
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {SideBarData} from './SideBarData'
+import * as FaIcons from 'react-icons/fa';
 
 interface SideBarProps {
 }
 
 interface SideBarState {
+  isOpen: boolean ;
 }
 
 
@@ -14,21 +16,41 @@ interface SideBarState {
  * SideBar component
  */
 export default class SideBar extends React.Component<SideBarProps, SideBarState> {  
-    render() {
-        return (
-            <nav className='nav-menu'>
-                <ul className='nav-menu-items'>
-                {SideBarData.map((item, index) => {
-                    return (
-                            <li key={index} className={item.cName}>
-                            <Link to={item.path}>
-                            <span>{item.title}</span>
-                            </Link>
-                            </li>
-                    );
-                })}
-                </ul>
-            </nav>
-        );
+  constructor(props: SideBarProps) {
+    super(props);
+    this.state = {
+      isOpen: false
     }
-}
+    this.showSidebar = this.showSidebar.bind(this)
+  }
+
+  showSidebar(isOpen: boolean){
+    if (isOpen === false) {
+      this.setState({isOpen: true})
+    }
+    else {
+      this.setState({isOpen: false})
+    }
+  }
+
+  render() {
+    return (
+      <nav className={this.state.isOpen ? 'nav-menu active' : 'nav-menu'}>
+        <Link to='#' className='menu-bars'>
+          <FaIcons.FaBars onClick={() => this.showSidebar(this.state.isOpen)} />
+        </Link>
+        <ul className={this.state.isOpen ? 'nav-menu-items active' : 'nav-menu-items'}>
+        {SideBarData.map((item, index) => {
+          return (
+                  <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                  <span>{item.title}</span>
+                  </Link>
+                  </li>
+          );
+        })}
+        </ul>
+      </nav>
+    );
+    }
+  }
