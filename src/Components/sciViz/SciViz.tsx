d87b8bb3ec83b87a6cb3fcff99b0b04add168aa8
A-Baji <bidabaji@gmail.com>
2023-02-03 17:19:29 -0600

    sciviz component

--- src/Components/sciViz/SciViz.tsx
+++ src/Components/sciViz/SciViz.tsx
@@ -1,5 +1,36 @@
-function SciViz() {
-    return <></>
+import { Tabs } from 'antd'
+import { SciVizSpec } from './SciVizInterfaces'
+import SciVizPage from './SciVizPage'
+
+interface SciVizProps {
+    jwtToken: string
+    sciviz: SciVizSpec
+}
+
+function SciViz(props: SciVizProps) {
+    const menuItems = Object.entries(props.sciviz.SciViz.pages).map(([name, page]) => ({
+        key: name,
+        label: (
+            <span
+                style={{
+                    display: 'flex',
+                    alignItems: 'center'
+                }}
+            >
+                <div>{name}</div>
+            </span>
+        ),
+        children: <SciVizPage jwtToken={props.jwtToken} page={page} />
+    }))
+    return (
+        <Tabs
+            centered
+            type='line'
+            size='large'
+            items={menuItems}
+            defaultActiveKey={menuItems[0].key}
+        />
+    )
 }
 
 export default SciViz
