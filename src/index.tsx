9457f82fdbe8d2797cfebf6c815e1c197de11685
jverswijver <jeroen.verswijver@gmail.com>
2023-02-27 10:45:17 -0600

    working TSC

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 import './index.css'
 import 'antd/dist/antd.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
+// import reportWebVitals from './reportWebVitals'
 
 const queryClient = new QueryClient()
 
@@ -22,4 +22,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+// reportWebVitals()
