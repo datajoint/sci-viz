cc7e0247e0611586e1d3e3cd89edc0e4e17de403
A-Baji <bidabaji@gmail.com>
2023-03-08 14:00:36 -0600

    iframe url qparam implementation

--- src/Components/IFrame/SciVizIFrame.tsx
+++ src/Components/IFrame/SciVizIFrame.tsx
@@ -10,13 +10,27 @@ interface iFrameProps {
 
 function SciVizIFrame(props: iFrameProps) {
     const { componentContext } = useContext(ExternalContext)
+    let url = props.url
+
+    if (url.indexOf('?') !== -1) {
+        url += `${componentContext ? '&context=' + JSON.stringify(componentContext) : ''}`
+        url += `${props.databaseHost ? '&database_host=' + props.databaseHost : ''}`
+    } else {
+        url += `${componentContext ? '?context=' + JSON.stringify(componentContext) : ''}`
+        if (url.indexOf('?') !== -1) {
+            url += `${props.databaseHost ? '&database_host=' + props.databaseHost : ''}`
+        } else {
+            url += `${props.databaseHost ? '?database_host=' + props.databaseHost : ''}`
+        }
+    }
+
     return (
         <Card
             style={{ width: '100%', height: props.height }}
             bodyStyle={{ height: '100%', padding: '5px' }}
             hoverable={true}
         >
-            <iframe width='100%' style={{ height: '100%' }} src={props.url} />
+            <iframe width='100%' style={{ height: '100%' }} src={url} />
         </Card>
     )
 }
